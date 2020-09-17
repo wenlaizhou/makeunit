1982d00723628f8498efed88416af28fc8723956
wenlai <wenlai_zhou@126.com>
2020-09-17 17:21:05 +0800

    修复代码

--- boot.go
+++ boot.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 // 1. 描述
@@ -20,24 +21,24 @@ ExecStart=%v
 Restart=always
 StartLimitInterval=0
 RestartSec=10
-WorkingDirectory=%v
+%v
 
 [Install]
 WantedBy=multi-user.target
 `
 
 func main() {
-	if len(os.Args) < 2 {
+	if len(os.Args) < 3 {
 		println("::::生成系统服务::::")
 		println("")
 		println("参数列表:")
 		println("1:服务名称, 2:执行命令(使用绝对路径表示命令位置, 使用\"\"将完整命令包括其中), 3:工作目录(绝对目录, 可为空)")
 		return
 	}
-	name, runnable := os.Args[1], os.Args[2]
+	name, runnable := strings.TrimSpace(os.Args[1]), strings.TrimSpace(os.Args[2])
 	dir := ""
 	if len(os.Args) > 3 {
-		dir = os.Args[3]
+		dir = fmt.Sprintf("WorkingDirectory=%v", strings.TrimSpace(os.Args[3]))
 	}
 	content := fmt.Sprintf(tpl, fmt.Sprintf("%v", name), runnable, dir)
 	err := ioutil.WriteFile(fmt.Sprintf("/usr/lib/systemd/system/%v.service", name), []byte(content), os.ModePerm)
